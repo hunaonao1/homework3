1cfdd30fb95501916e437b141ae393424bf9e4f6
songying <740660604@qq.com>
2016-07-04 17:37:48 +0800

    fighting3

--- homework3/__init__.py
+++ homework3/__init__.py
@@ -3,11 +3,15 @@
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.config.from_pyfile('app.conf')
 db = SQLAlchemy(app)
+app.secret_key='Mayday'
+login_manager=LoginManager(app)
+login_manager.login_view='/regloginpage'
 
 from homework3 import views, models
 
--- manage.py
+++ manage.py
@@ -15,10 +15,10 @@ def init_database():
     db.create_all()
     for i in range(0, 100):
         db.session.add(User('user' +str(i+1), 'a'+str(i)))
-        for j in range(0,3):
+        for j in range(0,10):
             db.session.add(Image(get_image_url(),i+1))
             for k in range(0,3):
-                db.session.add(Comment('This is a comment'+str(k),1+3*i+j,i+1))
+                db.session.add(Comment('This is a comment'+str(k),1+10*i+j,i+1))
     db.session.commit()
 
     for i in range(50,100,2):
